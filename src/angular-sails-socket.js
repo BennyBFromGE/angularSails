cd3d264080306b363ef52b49b75cb38daaa61801
Rob Wormald <robwormald@gmail.com>
2014-03-13 23:35:43 -0500

    slight refactor of socket provider
    module pattern : '$get' vs this.$get, for consistency w/ other ngSails
    modules.

--- src/angular-sails-socket.js
+++ src/angular-sails-socket.js
@@ -4,7 +4,7 @@ angular.module('angularSails.io', [])
   /**
    * sailsSocketFactory
    *
-   * sailsAngular wrapper around socket.io 
+   * sailsAngular wrapper around socket.io
    *
    * Code from :
    *
@@ -15,152 +15,161 @@ angular.module('angularSails.io', [])
    *
    * sails.io.js by Balderdashy : https://github.com/balderdashy/sails
    */
+    .provider('sailsSocketFactory', function() {
 
-.provider('sailsSocketFactory', function() {
-    var defaultPrefix = 'sails:',
-      ioSocket;
+        //default prefix for events forwarded to angular scopes.
+        // eg - $rootScope.$on('sails:foobar',function(ev,data){})
+        var defaultPrefix = 'sails:';
 
-    // expose to provider
-    this.$get = ['$q', '$rootScope', '$timeout', '$window',
-      function($q, $rootScope, $timeout, $window) {
+        //placeholder to allow passing in an existing socket.io socket.
+        var ioSocket = undefined;
 
-        //plugs a socket emit into the ngDigest cycle.
-        var asyncAngularify = function(socket, callback) {
-          return callback ? function() {
-            var args = arguments;
-            $timeout(function() {
-              callback.apply(socket, args);
-            }, 0);
-          } : angular.noop;
+        // expose to provider
+        return {
+            '$get' : ['$q', '$rootScope', '$timeout', '$window',
+                function($q, $rootScope, $timeout, $window) {
+
+                    //plugs a socket emit into the ngDigest cycle.
+                    var asyncAngularify = function(socket, callback) {
+                        return callback ? function() {
+                            var args = arguments;
+                            $timeout(function() {
+                                callback.apply(socket, args);
+                            }, 0);
+                        } : angular.noop;
+                    };
+
+
+                    //simulates an Sails $http request over a socket.io connection.
+                    var sailsRequest = function(socket, method, url, data) {
+
+                        var sailsDeferredRequest = $q.defer()
+
+                        url = url.replace(/^(.+)\/*\s*$/, '$1');
+                        // If method is undefined, use 'get'
+                        method = method || 'get';
+
+                        if (typeof url !== 'string') {
+                            sailsDeferredRequest.reject(new Error('Invalid or missing URL!\n' + usage));
+                        }
+
+                        //sails accepts requests formatted as {url : '/api/foos', data: {id : 1}} over websockets
+                        var requestJson = io.JSON.stringify({
+                            url: url,
+                            data: data
+                        });
+
+
+                        // Send the message over the socket
+                        socket.emit(method, requestJson, asyncAngularify(socket, function afterEmitted(result) {
+                            var parsedResult = result;
+                            if (result && typeof result === 'string') {
+                                try {
+                                    parsedResult = io.JSON.parse(result);
+                                } catch (e) {
+                                    if (typeof console !== 'undefined') {
+                                        console.warn("Could not parse:", result, e);
+                                    }
+                                    return sailsDeferredRequest.reject("Server response could not be parsed!\n" + result);
+                                }
+                            }
+
+
+                            if (parsedResult.status == 404) return sailsDeferredRequest.reject(new Error("404: Not found"));
+                            if (parsedResult.status == 403) return sailsDeferredRequest.reject(new Error("403: Forbidden"));
+                            if (parsedResult.status == 500) return sailsDeferredRequest.reject(new Error("500: Server Error"));
+
+                            else return sailsDeferredRequest.resolve(parsedResult)
+                        }));
+                        return sailsDeferredRequest.promise;
+                    }
+
+
+                    return function sailsSocket(options) {
+
+                        options = options || {};
+
+                        //allow connection to remote APIs
+                        var url = options.baseUrl || $window.location.origin || 'http://localhost:1337'
+
+                        //check if options contains a token for JWT.
+                        if (options.token) {
+                            var authString = "?token=" + options.token
+                            url = url + authString
+                        }
+
+                        //connect the socket.
+                        var socket = options.ioSocket || io.connect(url);
+
+                        //prefix for forwarding
+                        var prefix = options.prefix || defaultPrefix;
+
+                        //default scope to apply to
+                        var defaultScope = options.scope || $rootScope;
+
+                        //adds a socket listenter
+                        var addListener = function(eventName, callback) {
+                            socket.on(eventName, asyncAngularify(socket, callback));
+                        };
+
+
+
+
+                        //a wrapper around a socket.io connection, to work with the sails API
+                        var sailsSocket = {
+
+                            on: addListener,
+
+                            addListener: addListener,
+
+                            emit: function(eventName, data, callback) {
+                                return socket.emit(eventName, data, asyncAngularify(socket, callback));
+                            },
+
+                            removeListener: function() {
+                                return socket.removeListener.apply(socket, arguments);
+                            },
+
+                            forward: function(events, scope) {
+                                if (events instanceof Array === false) {
+                                    events = [events]
+                                }
+                                if (!scope) {
+                                    scope = defaultScope;
+                                }
+                                events.forEach(function(eventName) {
+                                    var prefixedEvent = prefix + eventName;
+
+                                    var forwardBroadcast = asyncAngularify(socket, function(data) {
+                                        scope.$broadcast(prefixedEvent, data)
+                                    });
+
+                                    scope.$on('$destroy', function() {
+                                        socket.removeListener(eventName, forwardBroadcast)
+                                    });
+
+                                    socket.on(eventName, forwardBroadcast);
+                                });
+                            },
+                            //sails REST API over socket.io with promises ftw
+                            get: function(path, query) {
+                                return sailsRequest(socket, 'get', path, query)
+                            },
+                            post: function(path, data) {
+                                return sailsRequest(socket, 'post', path, data)
+                            },
+                            put: function(path, data) {
+                                return sailsRequest(socket, 'put', path, data)
+                            },
+                            delete: function(path, data) {
+                                return sailsRequest(socket, 'delete', path, data)
+                            }
+                        };
+
+                        return sailsSocket
+                    };
+                }]
         };
-
-
-        //simulates an Sails $http request over a socket.io connection.
-        var sailsRequest = function(socket, method, url, data) {
-
-          var sailsDeferredRequest = $q.defer()
-
-          url = url.replace(/^(.+)\/*\s*$/, '$1');
-          // If method is undefined, use 'get'
-          method = method || 'get';
-
-          if (typeof url !== 'string') {
-            sailsDeferredRequest.reject(new Error('Invalid or missing URL!\n' + usage));
-          }
-
-          //sails accepts requests formatted as {url : '/api/foos', data: {id : 1}} over websockets
-          var requestJson = io.JSON.stringify({
-            url: url,
-            data: data
-          });
-
-          // Send the message over the socket
-          socket.emit(method, requestJson, asyncAngularify(socket, function afterEmitted(result) {
-            var parsedResult = result;
-            if (result && typeof result === 'string') {
-              try {
-                parsedResult = io.JSON.parse(result);
-              } catch (e) {
-                if (typeof console !== 'undefined') {
-                  console.warn("Could not parse:", result, e);
-                }
-                return sailsDeferredRequest.reject("Server response could not be parsed!\n" + result);
-              }
-            }
-
-
-            if (parsedResult.status == 404) return sailsDeferredRequest.reject(new Error("404: Not found"));
-            if (parsedResult.status == 403) return sailsDeferredRequest.reject(new Error("403: Forbidden"));
-            if (parsedResult.status == 500) return sailsDeferredRequest.reject(new Error("500: Server Error"));
-
-            else return sailsDeferredRequest.resolve(parsedResult)
-          }));
-          return sailsDeferredRequest.promise;
-        }
-
-
-        return function sailsSocket(options) {
-
-          options = options || {};
-
-          //allow connection to remote APIs
-          var url = $window.location.origin || 'http://localhost:1337'
-
-          //check if options contains a token for JWT.
-          if (options.token) {
-            var authString = "?token=" + options.token
-            url = url + authString
-          }
-
-          //connect the socket.
-          var socket = options.ioSocket || io.connect(url);
-
-          //prefix for forwarding
-          var prefix = options.prefix || defaultPrefix;
-
-          //default scope to apply to
-          var defaultScope = options.scope || $rootScope;
-
-          //adds a socket listenter
-          var addListener = function(eventName, callback) {
-            socket.on(eventName, asyncAngularify(socket, callback));
-          };
-
-          //a wrapper around a socket.io connection, to work with the sails API
-          var sailsSocket = {
-
-            on: addListener,
-
-            addListener: addListener,
-
-            emit: function(eventName, data, callback) {
-              return socket.emit(eventName, data, asyncAngularify(socket, callback));
-            },
-
-            removeListener: function() {
-              return socket.removeListener.apply(socket, arguments);
-            },
-
-            forward: function(events, scope) {
-              if (events instanceof Array === false) {
-                events = [events]
-              }
-              if (!scope) {
-                scope = defaultScope;
-              }
-              events.forEach(function(eventName) {
-                var prefixedEvent = prefix + eventName;
-
-                var forwardBroadcast = asyncAngularify(socket, function(data) {
-                  scope.$broadcast(prefixedEvent, data)
-                });
-
-                scope.$on('$destroy', function() {
-                  socket.removeListener(eventName, forwardBroadcast)
-                });
-
-                socket.on(eventName, forwardBroadcast);
-              });
-            },
-            //sails REST API over socket.io with promises ftw
-            get: function(path, query) {
-              return sailsRequest(socket, 'get', path, query)
-            },
-            post: function(path, data) {
-              return sailsRequest(socket, 'post', path, data)
-            },
-            put: function(path, data) {
-              return sailsRequest(socket, 'put', path, data)
-            },
-            delete: function(path, data) {
-              return sailsRequest(socket, 'delete', path, data)
-            }
-          };
-
-          return sailsSocket
-        };
-      }
-    ];
 })
 
   //decorate $q so we can use success/error
